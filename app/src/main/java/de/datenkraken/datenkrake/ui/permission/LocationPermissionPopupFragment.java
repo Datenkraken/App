019ced6e4bb19887c5072a47cb8e3ab05c87687c
Daniel Thoma <daniel.thoma@stud.tu-darmstadt.de>
2020-05-18 11:17:12 +0200

    add location request

---
+++ app/src/main/java/de/datenkraken/datenkrake/ui/permission/LocationPermissionPopupFragment.java
@@ -0,0 +1,72 @@
+package de.datenkraken.datenkrake.ui.permission;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.browser.customtabs.CustomTabsIntent;
+import androidx.fragment.app.DialogFragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.datenkraken.datenkrake.R;
+import timber.log.Timber;
+
+public class LocationPermissionPopupFragment extends DialogFragment {
+
+    @BindView(R.id.location_permission_request_popup_text)
+    TextView dataCollectionText;
+    @BindView(R.id.location_permission_request_accept_button)
+    Button acceptButton;
+    @BindView(R.id.location_permission_request_cancel_button)
+    Button cancelButton;
+
+    private final LocationPermissionPopupViewModel locationPermissionPopupViewModel = new LocationPermissionPopupViewModel();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        Window window = Objects.requireNonNull(getDialog()).getWindow();
+        Objects.requireNonNull(window).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.location_permission_request_popup_fragment,
+            null,
+            false);
+        ButterKnife.bind(this, view);
+
+        // Set Buttons
+        acceptButton.setOnClickListener(v -> {
+            locationPermissionPopupViewModel.openSystemPermissionHandler(getActivity());
+            dismiss();
+        });
+        cancelButton.setOnClickListener(v -> {
+            locationPermissionPopupViewModel.save(Objects.requireNonNull(getContext()), false);
+            dismiss();
+        });
+
+        // Set Builder and Buttons
+        return builder.setView(view)
+            .show();
+    }
+}
---
+++ app/src/main/java/de/datenkraken/datenkrake/ui/permission/LocationPermissionPopupViewModel.java
@@ -0,0 +1,33 @@
+package de.datenkraken.datenkrake.ui.permission;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModel;
+import de.datenkraken.datenkrake.R;
+import timber.log.Timber;
+
+class LocationPermissionPopupViewModel extends ViewModel {
+
+    void save(Context context, boolean status) {
+        SharedPreferences sharedPreferences =
+            context.getSharedPreferences(context.getString(R.string.preference_permission),
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(context.getString(R.string.preference_permission_location), status);
+        editor.apply();
+
+    }
+
+    void openSystemPermissionHandler(Activity activity) {
+        ActivityCompat.requestPermissions(activity,
+            new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+            activity.getResources().getInteger(R.integer.permission_fine_location));
+
+    }
+}
